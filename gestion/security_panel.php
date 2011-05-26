21f6fbc11967ab613476c1e73b625fe0a098df18
crob <crob@e073496e-d129-0410-8f7b-841bfd467322>
2011-05-26 11:29:54 +0000

    Rétablissement des champs de filtrage.
    git-svn-id: http://svn.sylogix.org/gepiphp/branches/release-1.5.4@7013 e073496e-d129-0410-8f7b-841bfd467322

--- gestion/security_panel.php
+++ gestion/security_panel.php
@@ -88,8 +88,8 @@ $test_arch=mysql_query($sql);
 if(mysql_num_rows($test_arch)>0) {
 	echo "<a href='security_panel_archives.php'>Historique des alertes sécurité</a>";
 }
-else {
-	echo "Historique des alertes sécurité";
+//else {
+//	echo "Historique des alertes sécurité";
 
 	if($tri=='nom') {
 		$sql="SELECT DISTINCT u.login, u.nom, u.prenom, u.statut, count(t.login) AS nb FROM utilisateurs u, tentatives_intrusion t WHERE t.login=u.login GROUP BY u.login ORDER BY u.nom, u.prenom;";
@@ -150,7 +150,7 @@ else {
 SELECT DISTINCT adresse_ip, count(adresse_ip) as nb FROM tentatives_intrusion GROUP BY adresse_ip ORDER BY nb DESC;
 
 */
-}
+//}
 echo "</p>\n";
 
 echo "<script type='text/javascript'>
